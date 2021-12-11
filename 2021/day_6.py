8c77d7ff41eebd1ed7e8a104f3252bf7da4a77d9
Dowsley <joao.dowsley850@gmail.com>
2021-12-11 15:53:44 -0300

    Day 6

--- 2021/day_6.py
+++ 2021/day_6.py
@@ -1,37 +1,42 @@
 import itertools
+from collections import OrderedDict
+
+class TimerGroup():
+    def __init__(self, clock, qty):
+        self.clock = clock
+        self.qty = qty
+    def __repr__(self):
+        return f"({self.clock}, {self.qty})"
 
 def get_input(filename):
     with open(filename, 'r') as f:
-        l = f.readlines()[0].strip().split(',')
-        return list(map(int, l))
+        lst = f.readlines()[0].strip().split(',')
+        lst = list(map(int, lst))
+        initial = [0 for i in range(0, 9)]
+        for c in lst:
+            initial[c] += 1
+    return initial
 
-def spawn_new_fish(timers, amount):
-    new_fish = [8 for i in range(amount)]
-    return timers + new_fish
+def rotate(l, n):
+    return l[n:] + l[:n]
 
 def tick_timers(timers):
-    amount = 0
-    for i, t in enumerate(timers):
-        if t==0:
-            timers[i] = 6
-            amount += 1
-        else:
-            timers[i] -= 1
-    return amount
+    new = rotate(timers, 1) # Shift to left
+    fish_to_reset = timers[0]
+    new[6] += fish_to_reset
+    return new
 
-def solve_part_1(inputs, n_days):
+def solve(inputs, n_days):
     timers = inputs.copy()
     for i in range(1, n_days+1):
-        print(i)
-        new_fish_amount = tick_timers(timers)
-        timers = spawn_new_fish(timers, new_fish_amount)
-    return len(timers)
+        timers = tick_timers(timers)
+    return sum(timers)
 
 if __name__ == '__main__':
     inputs = get_input('input/day_6.txt')
-
-    answer1 = solve_part_1(inputs, 80)
+    answer1 = solve(inputs, 80)
     print(f"Part 1: {answer1}")
 
-    answer2 = solve_part_1(inputs, 256)
+    inputs = get_input('input/day_6.txt')
+    answer2 = solve(inputs, 256)
     print(f"Part 2: {answer2}")
