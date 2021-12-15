92a00d7e4a22b4b297415a98752c0ab83d496943
João Dowsley <joao.dowsley850@gmail.com>
2021-12-15 16:27:43 -0300

    wip

--- 2021/day_8.py
+++ 2021/day_8.py
@@ -10,12 +10,65 @@ def process_input(inp):
     return out
 
 def solve_1(inputs):
+    d = [2,3,4,7]
+
+    out = 0
     for l in inputs:
-        p = process_input(l)
-        print(p)
+        p = process_input(l)[1]
+        for w in p:
+            if len(w) in d:
+                out +=1
+    return out
+
+def decode(encoded_digits):
+    originals_by_n = { # DIGIT: Original segments set
+        "0": set("abcefg"),
+        "1": set("cf"),
+        "2": set("acdeg"),
+        "3": set("acdfg"),
+        "4": set("bcdf"),
+        "5": set("abdfg"),
+        "6": set("abdefg"),
+        "7": set("acf"),
+        "8": set("abcdefg"),
+        "9": set("abcdfg"),
+    }
+    # LENGTH: Digit
+    originals_by_len = {}
+    for k, v in originals_by_n.items():
+        l = len(v)
+        if not originals_by_len.get(l):
+            originals_by_len[l] = list()
+        originals_by_len[l].append(v)
+
+    # DECODE!
+    maps = []
+    encoded_digits.sort(key=len)
+    encoded_digits = list(map(set, encoded_digits))
+
+    # Step 1: Decode segments from unique numbers 1,7,4,8
+    uniques = encoded_digits[:3] + [encoded_digits[-1]]
+    for encoded in uniques:
+        original = originals_by_len[len(encoded)][0].copy()
+        encoded = encoded.copy()
+        # Get combinations
+        for e, o in maps:
+            original = original.difference(o)
+            encoded = encoded.difference(e)
+        maps.append([encoded, original])
+
+    # Step 2: Decode segments from non-unique numbers
+    non_uniques = encoded_digits[4:][:-1]
+    for encoded in non_uniques:
+        tmp = encoded.copy()
+    print(non_uniques)
 
 def solve_2(inputs):
-    pass
+    res = []
+
+    for i in inputs:
+        all_digits, display_digits = process_input(i)
+        mapping = decode(all_digits)
 
 if __name__ == '__main__':
     inputs = get_input('input/day_8.txt')
